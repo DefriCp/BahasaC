3602ee11000791d7cd2e26c4d3ee05673faa1170
DefriCp <62134875+DefriCp@users.noreply.github.com>
2020-03-13 16:11:10 +0700

    Create Mencoba.c

---
+++ Mencoba.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{ /* Kamus */
+int usia = 20;
+float berat = 73.878;
+double tinggi = 170.8;
+char Jenis_Kelamin = 'L';
+
+
+printf("Usia: %i tahun\n", usia);
+printf("Tinggi Badan: %.2f CM\n", tinggi );
+printf("Berat Badan: %.3f KG\n", berat);
+printf("Jenis Kelamin: %c\n", Jenis_Kelamin);
+return 0;
+}
